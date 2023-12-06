0af27a7f20c3747aca512910dc6de71fbfec99d7
owusuoppongemmanuel <50124632+owusuoppongemmanuel@users.noreply.github.com>
2023-12-06 08:39:23 +0800

    up

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 
 if __name__ == "__main__":
     from add_0 import add
-a=1
-b=2
-print("{} + {} = {}".format(a=1, b=2, add(1,2)))
+    a=1
+    b=2
+    c=add(a,b)
+    print("{a} + {b} = {c}".format(a, b, c)
