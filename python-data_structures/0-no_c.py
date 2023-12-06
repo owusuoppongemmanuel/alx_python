b2bed02cdfd14cee5f8b89429cf0fb4b883993a4
owusuoppongemmanuel <50124632+owusuoppongemmanuel@users.noreply.github.com>
2023-12-06 08:59:30 +0800

    update and

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
    string = ""
    for char in my_string:
-    if char in my_string != "c" or char != "C":
+    if char in my_string != "c" and char != "C":
         string += char
     return string
