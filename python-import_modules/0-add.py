314cdf28ba8fbd346b0cfb2ef314248ced9e87c4
owusuoppongemmanuel <50124632+owusuoppongemmanuel@users.noreply.github.com>
2023-12-06 08:17:03 +0800

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
-def add(a,b):
-    a=1
-    b=1
-    return(a+b)
 
 if __name__ == "__main__":
     from add_0 import add
-    a=1
-    b=2
+   print("a=1") 
+   print("b=2")
     print("{} + {} = {}".format(1, 2, add(1,2)))
