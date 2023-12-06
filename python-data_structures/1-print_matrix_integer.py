96b4b7975667876888379d953d185d00583270df
owusuoppongemmanuel <50124632+owusuoppongemmanuel@users.noreply.github.com>
2023-12-06 10:23:27 +0800

    just a check

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
+    """This function prints a matrix of integers."""
     for row in matrix:
-        for element in matrix:
-            print("{:d}".format(element))
-
-print()
+        for element in row:
+            print(f"{element:3}", end=" ")
+        print()
