4014ce3c0b6b64763505819226abce0e999ac549
owusuoppongemmanuel <50124632+owusuoppongemmanuel@users.noreply.github.com>
2023-12-06 10:31:55 +0800

    check

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,6 @@
 def multiple_returns(sentence):
     first-letter=" "
     if len(sentence)>0:
-       return(first_letter.append(sentence[0]))
+       return tuple((first_letter.append(sentence[0])))
     else:
         return("None")
